e7e8877c779d22cb1c55d1ab74dac36022296c89
Ardhani Dzaky Ralfiano <82014329+ardhanidzaky@users.noreply.github.com>
2024-06-09 14:29:34 +0700

    Create category_dm_spark.py

---
+++ dags/category_dm_spark.py
@@ -0,0 +1,64 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import datetime
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count, col, date_format
+import os
+
+def main():
+    os.environ['PYSPARK_PYTHON'] = '/Users/ardhanidzaky/Documents/celerates/cele_env/bin/python'
+    os.environ['PYSPARK_DRIVER_PYTHON'] = '/Users/ardhanidzaky/Documents/celerates/cele_env/bin/python'
+
+    src_hook = PostgresHook(postgres_conn_id='supabase')
+    src_conn = src_hook.get_conn()
+    src_cursor = src_conn.cursor()
+
+    current_date = datetime.date.today().strftime('%Y-%m-%d')
+    query = f"""
+        SELECT  *
+        FROM    transactional
+        WHERE   pt_date = '{current_date}'
+                AND date(date) = '{current_date}';
+    """
+    src_cursor.execute(query)
+    data = src_cursor.fetchall()
+    columns = [desc[0] for desc in src_cursor.description]
+    df = pd.DataFrame(data, columns=columns)
+
+    # Initialize Spark session
+    spark = SparkSession.builder \
+        .appName("Create Category Data Mart") \
+        .getOrCreate()
+    transactional_df = spark.createDataFrame(df)
+
+    # Perform transformation
+    data_mart1_df = transactional_df \
+        .groupBy(date_format(col("date"), "yyyy-MM-dd").alias("date"), "category") \
+        .agg(count("*").alias("request_count"))
+    
+    # Convert Spark DataFrame to Pandas DataFrame
+    result_df = data_mart1_df.toPandas()
+
+    # Connect to PostgreSQL using PostgresHook for writing
+    dest_hook = PostgresHook(postgres_conn_id='supabase')
+    dest_conn = dest_hook.get_conn()
+    dest_cursor = dest_conn.cursor()
+
+    # Append data to the target table in PostgreSQL
+    for _, row in result_df.iterrows():
+        insert_query = """
+            INSERT INTO category_pd (date, category, request_count)
+            VALUES (%s, %s, %s);
+        """
+        dest_cursor.execute(insert_query, (row['date'], row['category'], row['request_count']))
+
+    dest_conn.commit()
+    dest_cursor.close()
+    dest_conn.close()
+    src_cursor.close()
+    src_conn.close()
+
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
